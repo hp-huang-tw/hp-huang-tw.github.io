6c541fcfa308af174e092c6155aaf3710d1416f7
hp.huang <stitchmos@gmail.com>
2018-10-28 17:20:48 +0800

    initial nuxtjs-2.2.0 from scratch

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+export default {
+  head: {
+    title: 'Anna Huang',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Anna Huang Profile' }
+    ]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <span>Hi, this is {{name}}!</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+     name: `Anna Huang`
+    }
+  }
+}
+</script>
